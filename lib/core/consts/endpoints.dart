da0fb202c857ac8dac0bf4e384b50023b4676205
Suman Raj Pathak <viper.suman@gmail.com>
2023-08-29 11:35:29 +0545

    fix:update server url

--- lib/core/consts/endpoints.dart
+++ lib/core/consts/endpoints.dart
@@ -1,5 +1,5 @@
 class Endpoints {
-  static String baseUrl = "https://create-cfnliked3q-de.a.run.app/";
+  static String baseUrl = "https://update-cfnliked3q-de.a.run.app/";
   static String pokemonDetail = "pokemons";
   static String pokemonList = "getlist";
 }
